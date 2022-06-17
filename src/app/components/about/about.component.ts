cf90ebcb1ae0a1bce7faa60969d064a654530a8e
dap177 <dap177@txstate.edu>
2022-06-16 22:17:46 -0500

    Implement about me page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  tagline: String = "Hello! I'm Drew, a software engineer currently based out of Houston, Texas."
+
+  bioText: String = "I recently graduated Summa Cum Laude from Texas State University with a major in Computer Science and a minor in Media Studies, and I now work as a Software Engineer at Slalom Build, where I work on outcome oriented software solutions in highly skilled teams for a diverse set of clients. \n\nI enjoy problem solving, and I constantly strive to collaborate, learn new things, and grow my skillset overall. Additionally, I'm driven to use best practices in all aspects of my career. \n\nIn my free time, I like to thrift, read, travel, and drink coffee!";
+
   constructor() { }
 
   ngOnInit(): void {
