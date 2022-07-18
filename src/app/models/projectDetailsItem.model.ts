e4a7ea2209fd6142af15784fe07f49c3b43a9533
dap177 <dap177@txstate.edu>
2022-07-17 21:38:43 -0500

    Clean up image assets and update models

--- src/app/models/projectDetailsItem.model.ts
+++ src/app/models/projectDetailsItem.model.ts
@@ -2,6 +2,9 @@ export interface ProjectDetailsItem {
     title: String,
     route: String,
     imagePath: String,
+    imagePath2?: String,
+    imagePath3?: String,
+    imagePath4?: String,
     description: String,
     link: String
 }
