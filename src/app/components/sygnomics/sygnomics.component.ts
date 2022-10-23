de0ef2f0f83d0e34f9239919b61b8c3eba85f58c
dap177 <dap177@txstate.edu>
2022-10-23 00:44:21 -0500

    Writing first project description

--- src/app/components/sygnomics/sygnomics.component.ts
+++ src/app/components/sygnomics/sygnomics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectDetailsItem } from 'src/app/models/projectDetailsItem.model';
 
 @Component({
   selector: 'app-sygnomics',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sygnomics.component.scss']
 })
 export class SygnomicsComponent implements OnInit {
+
+  projectDetails: ProjectDetailsItem = {
+    title: "Sygnomics Internship Project — Summer 2021",
+    link: "https://www.slalombuild.com/our-work/sygnomics"
+  }
 
   constructor() { }
 
--- src/app/models/projectDetailsItem.model.ts
+++ src/app/models/projectDetailsItem.model.ts
@@ -1,10 +1,8 @@
 export interface ProjectDetailsItem {
     title: String,
-    route: String,
-    imagePath: String,
+    imagePath?: String,
     imagePath2?: String,
     imagePath3?: String,
     imagePath4?: String,
-    description: String,
-    link: String
+    link?: String
 }
