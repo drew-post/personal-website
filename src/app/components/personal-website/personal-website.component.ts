b3dbdf774765bbf89c1aa82e3347947d7b219453
drew-post <drew_post@outlook.com>
2022-12-18 22:56:57 -0600

    Updating model and personal website component

--- src/app/components/personal-website/personal-website.component.ts
+++ src/app/components/personal-website/personal-website.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectDetailsItem } from 'src/app/models/projectDetailsItem.model';
 
 @Component({
   selector: 'pw-personal-website',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personal-website.component.scss']
 })
 export class PersonalWebsiteComponent implements OnInit {
+
+  projectDetails: ProjectDetailsItem = {
+    title: "My Personal Website",
+    techDescription: "Angular, Typescript, Github Pages",
+  }
 
   constructor() { }
 
--- src/app/models/projectDetailsItem.model.ts
+++ src/app/models/projectDetailsItem.model.ts
@@ -1,9 +1,5 @@
 export interface ProjectDetailsItem {
     title: String,
     techDescription: String,
-    imagePath?: String,
-    imagePath2?: String,
-    imagePath3?: String,
-    imagePath4?: String,
     link?: String
 }
