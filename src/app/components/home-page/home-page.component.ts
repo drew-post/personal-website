326df0033b686c9694e966d8d7d8f3a2199ba2d3
drew-post <drew_post@outlook.com>
2023-03-01 11:22:11 -0600

    Add individual homepage images and zoom on hover

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  backgroundImages1: String[] = ['homepage-1.png', 'homepage-2.png', 'homepage-3.png', 'homepage-4.png', 'homepage-5.png', 'homepage-6.png'];
+  backgroundImages2: String[] = ['homepage-7.png', 'homepage-8.png', 'homepage-9.png', 'homepage-10.png', 'homepage-11.png', 'homepage-12.png'];
+
   constructor() { }
 
   ngOnInit(): void {
