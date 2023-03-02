0be8e89dfc7fbe190fccb1015c3a70f0978d15f3
drew-post <drew_post@outlook.com>
2023-03-02 16:52:32 -0600

    Styling updates

--- src/app/components/sygnomics/sygnomics.component.ts
+++ src/app/components/sygnomics/sygnomics.component.ts
@@ -10,7 +10,7 @@ export class SygnomicsComponent implements OnInit {
 
   projectDetails: ProjectDetailsItem = {
     title: "Slalom Build: Sygnomics Internship Project",
-    techDescription: ['React', 'TypeScript', 'Amazon DynamoDB', 'Amazon Simple Storage Service (Amazon S3)', 'AWS Lambda', 'Amazon CloudFront', 'Amazon Cognito'],
+    techDescription: ['React', 'Node.js', 'TypeScript', 'Amazon DynamoDB', 'Amazon Simple Storage Service (Amazon S3)', 'AWS Lambda', 'Amazon CloudFront', 'Amazon Cognito'],
     link: "https://www.slalombuild.com/our-work/sygnomics"
   }
 
